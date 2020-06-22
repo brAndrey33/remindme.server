13393652a56f27e501fcb54a8fa6baddfa9502be
brAndrey33 <train1284@mail.ru>
2020-06-22 16:09:57 +0300

    lesson2 compleet

--- src/main/java/org/example/remindme/server/controller/ReminderController.java
+++ src/main/java/org/example/remindme/server/controller/ReminderController.java
@@ -1,15 +1,18 @@
 package org.example.remindme.server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/Remind")
+@RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get",method = RequestMethod.GET)
-    public String getReminder(){
+    @ResponseBody
+    public String getReminder(ModelMap model){
         return "May Reminder";
     }
 
